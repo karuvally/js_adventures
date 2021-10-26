d4461548b4636fd7f2379c7d0f6d3ec4c13b34a3
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-26 18:44:18 +0530

    multiple vars declaration

---
+++ multiple_vars/script.js
@@ -0,0 +1,18 @@
+/*
+This is a multiline comment
+It seems JS is friends with the C language
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Store a person's details
+let name = 'Aswin';
+let age = 40;
+let occupation = 'Programmer';
+
+// The message to be shown as alert
+let message = 'സന്മനസ്സുള്ളവർക്ക് സമാധാനം';
+
+// A simple Hello World statement
+alert(message);
