72c20581101fc7193465915a274b3f58a00a8325
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-26 16:36:47 +0530

    sample page using strict feature

---
+++ strict/script.js
@@ -0,0 +1,10 @@
+/*
+This is a multiline comment
+It seems JS is friends with the C language
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// A simple Hello World statement
+alert('Hello world!');
